9d84e001646d3c657a89176a15999fbfd6f4144a
banjich <banjich@gmail.com>
2021-09-21 09:17:56 +0200

    Added file index.js

---
+++ ex14/src/index.js
@@ -0,0 +1,13 @@
+function convertStrToInt(str) {
+    
+    return parseInt(str)
+}
+
+convertStrToInt();
+
+console.log(convertStrToInt(44));
+console.log(convertStrToInt(44));
+console.log(convertStrToInt(27));
+console.log(convertStrToInt("JeffBezos"));
+
+module.exports = convertStrToInt;
